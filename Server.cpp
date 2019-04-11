e523acf574e11d1d4087f2fdc2dd508fd8a30467
Luis Tapia <tapia641@gmail.com>
2019-04-11 12:35:40 -0500

    Entregado

--- Server.cpp
+++ Server.cpp
@@ -1,122 +1,131 @@
-	#include <fcntl.h>
-	#include <string.h>
-	#include <stdlib.h>
-	#include <errno.h>
-	#include <stdio.h>
-	#include <netinet/in.h>
-	#include <resolv.h>
-	#include <sys/socket.h>
-	#include <arpa/inet.h>
-	#include <unistd.h>
-	#include <pthread.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <resolv.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
 
-	void* SocketHandler(void*);
+void *SocketHandler(void *);
 
-	int main(int argv, char** argc){
+int main(int argv, char **argc)
+{
 
-		int host_port= 1101;
+	int host_port = 1101;
 
-		struct sockaddr_in my_addr;
+	struct sockaddr_in my_addr;
 
-		int hsock;
-		int * p_int ;
-		int err;
+	int hsock;
+	int *p_int;
+	int err;
 
-		socklen_t addr_size = 0;
-		int* csock;
-		sockaddr_in sadr;
-		pthread_t thread_id=0;
+	socklen_t addr_size = 0;
+	int *csock;
+	sockaddr_in sadr;
+	pthread_t thread_id = 0;
 
+	hsock = socket(AF_INET, SOCK_STREAM, 0);
+	if (hsock == -1)
+	{
+		printf("Error initializing socket %d\n", errno);
+		goto FINISH;
+	}
 
-		hsock = socket(AF_INET, SOCK_STREAM, 0);
-		if(hsock == -1){
-			printf("Error initializing socket %d\n", errno);
-			goto FINISH;
-		}
-		
-		p_int = (int*)malloc(sizeof(int));
-		*p_int = 1;
-			
-		if( (setsockopt(hsock, SOL_SOCKET, SO_REUSEADDR, (char*)p_int, sizeof(int)) == -1 )||
-			(setsockopt(hsock, SOL_SOCKET, SO_KEEPALIVE, (char*)p_int, sizeof(int)) == -1 ) ){
-			printf("Error setting options %d\n", errno);
-			free(p_int);
-			goto FINISH;
-		}
+	p_int = (int *)malloc(sizeof(int));
+	*p_int = 1;
 
+	if ((setsockopt(hsock, SOL_SOCKET, SO_REUSEADDR, (char *)p_int, sizeof(int)) == -1) ||
+		(setsockopt(hsock, SOL_SOCKET, SO_KEEPALIVE, (char *)p_int, sizeof(int)) == -1))
+	{
+		printf("Error setting options %d\n", errno);
 		free(p_int);
-
-		my_addr.sin_family = AF_INET ;
-		my_addr.sin_port = htons(host_port);
-		
-		memset(&(my_addr.sin_zero), 0, 8);
-		my_addr.sin_addr.s_addr = INADDR_ANY ;
-		
-		if( bind( hsock, (sockaddr*)&my_addr, sizeof(my_addr)) == -1 ){
-			fprintf(stderr,"Error binding to socket, make sure nothing else is listening on this port %d\n",errno);
-			goto FINISH;
-		}
-		if(listen( hsock, 10) == -1 ){
-			fprintf(stderr, "Error listening %d\n",errno);
-			goto FINISH;
-		}
-
-		//Now lets do the server stuff
-		addr_size = sizeof(sockaddr_in);
-
-		while(true){
-
-			csock = (int*)malloc(sizeof(int));
-		
-			if((*csock = accept( hsock, (sockaddr*)&sadr, &addr_size))!= -1){
-				printf("---------------------\nReceived connection from %s\n",inet_ntoa(sadr.sin_addr));
-				pthread_create(&thread_id,0,&SocketHandler, (void*)csock);
-				pthread_detach(thread_id);
-			}
-			else{
-				fprintf(stderr, "Error accepting %d\n", errno);
-			}
-			printf("waiting for a connection\n");
-		}
-
-	FINISH:
-	;
+		goto FINISH;
 	}
 
-	void* SocketHandler(void *lp){
-	    int *csock = (int*)lp;
+	free(p_int);
 
-		char buffer[1024];
-		int buffer_len = 1024;
-		int bytecount;
+	my_addr.sin_family = AF_INET;
+	my_addr.sin_port = htons(host_port);
 
-		do{
-			memset(buffer, 0, buffer_len);
-				if((bytecount = recv(*csock, buffer, buffer_len, 0))== -1){
-					fprintf(stderr, "Error receiving data %d\n", errno);
-					goto FINISH;
-				}
-			printf("Received bytes %d\nReceived string \"%s\"\n", bytecount, buffer);
-			
-			if (!strcmp(buffer,"SALIR"))
-			{
-				printf(" salio\n");
-				free(csock);
-				//break;
-				//goto FINISH;
-			}
+	memset(&(my_addr.sin_zero), 0, 8);
+	my_addr.sin_addr.s_addr = INADDR_ANY;
 
-			//strcat(buffer, " SERVER ECHO");
-			if((bytecount = send(*csock, buffer, strlen(buffer), 0))== -1){
-				fprintf(stderr, "Error sending data %d\n", errno);
-				goto FINISH;
-			}
-			
-			printf("Sent bytes %d\n", bytecount);
-
-		}while(bytecount > 0);
-			
-	FINISH:
-		free(csock);
-	    return 0;
+	if (bind(hsock, (sockaddr *)&my_addr, sizeof(my_addr)) == -1)
+	{
+		fprintf(stderr, "Error binding to socket, make sure nothing else is listening on this port %d\n", errno);
+		goto FINISH;
 	}
+	if (listen(hsock, 10) == -1)
+	{
+		fprintf(stderr, "Error listening %d\n", errno);
+		goto FINISH;
+	}
+
+	//Now lets do the server stuff
+	addr_size = sizeof(sockaddr_in);
+
+	while (true)
+	{
+		csock = (int *)malloc(sizeof(int));
+
+		if ((*csock = accept(hsock, (sockaddr *)&sadr, &addr_size)) != -1)
+		{
+			printf("---------------------\nReceived connection from %s\n", inet_ntoa(sadr.sin_addr));
+			pthread_create(&thread_id, 0, &SocketHandler, (void *)csock);
+			pthread_detach(thread_id);
+		}
+		else
+		{
+			fprintf(stderr, "Error accepting %d\n", errno);
+		}
+		printf("waiting for a connection\n");
+	}
+
+FINISH:;
+}
+
+void *SocketHandler(void *lp)
+{
+	int *csock = (int *)lp;
+
+	char buffer[1024];
+	int buffer_len = 1024;
+	int bytecount;
+
+	do
+	{
+
+		memset(buffer, 0, buffer_len);
+		if ((bytecount = recv(*csock, buffer, buffer_len, 0)) == -1)
+		{
+			fprintf(stderr, "Error receiving data %d\n", errno);
+			goto FINISH;
+		}
+
+		if (!strcmp(buffer, "SALIR") || bytecount == 0)
+		{
+			printf("SALIO\n");
+			break;
+		}
+
+		printf("Received bytes %d\nReceived string \"%s\"\n", bytecount, buffer);
+
+		//strcat(buffer, " SERVER ECHO");
+		if ((bytecount = send(*csock, buffer, strlen(buffer), 0)) == -1)
+		{
+			fprintf(stderr, "Error sending data %d\n", errno);
+			goto FINISH;
+		}
+
+		printf("Sent bytes %d\n", bytecount);
+
+	} while (bytecount > 0);
+
+FINISH:
+	free(csock);
+	return 0;
+}
